2891862041570cbaf18fd9dd051106db8d7a1aa6
Mark <meichangliang@outlook.com>
2020-03-21 00:30:05 +0800

    test  ~

--- test/testPIL.py
+++ test/testPIL.py
@@ -1,23 +1,3 @@
-# DrawChart
-
-Python 图片转字符串小工具
-
-## 用到的库
-
-### pillow
-
-```bash
-# 安装
-pip install pillow
-
-
-
-```
-
-例子
-
-```python
-
 from PIL import Image
 # 打开一个jpg图像文件，注意是当前路径:
 im = Image.open('test.jpg')
@@ -29,7 +9,3 @@ im.thumbnail((w//2, h//2))
 print('Resize image to: %sx%s' % (w//2, h//2))
 # 把缩放后的图像用jpeg格式保存:
 im.save('thumbnail.jpg', 'jpeg')
-
-```
-
-### argparse
